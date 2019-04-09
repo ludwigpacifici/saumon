aca7e764cecfdbd5776de589e217e57884fb3034
Ludwig PACIFICI <ludwig@lud.cc>
2019-04-09 19:48:35 +0100

    Added to_string to Token_kind

--- lib/scanner.mli
+++ lib/scanner.mli
@@ -1,7 +1,5 @@
 type t
 
-val make : unit -> t
-
 val scan_tokens : string -> Token.t list * bool
 
 val show : t -> string
--- lib/token_kind.ml
+++ lib/token_kind.ml
@@ -1,53 +1,82 @@
 type t =
-  | Left_paren
-  | Right_paren
-  | Left_brace
-  | Right_brace
+  | And
+  | Bang
+  | Bang_equal
+  | Class
   | Comma
   | Dot
+  | Else
+  | Eof
+  | Equal
+  | Equal_equal
+  | False
+  | For
+  | Fun
+  | Greater
+  | Greater_equal
+  | Identifier of string
+  | If
+  | Left_brace
+  | Left_paren
+  | Less
+  | Less_equal
   | Minus
+  | Nil
+  | Number of float
+  | Or
   | Plus
+  | Print
+  | Return
+  | Right_brace
+  | Right_paren
   | Semicolon
   | Slash
   | Star
-  | Bang
-  | Bang_equal
-  | Equal
-  | Equal_equal
-  | Greater
-  | Greater_equal
-  | Less
-  | Less_equal
-  | Identifier of string
   | String of string
-  | Number of float
-  | And
-  | Class
-  | Else
-  | False
-  | Fun
-  | For
-  | If
-  | Nil
-  | Or
-  | Print
-  | Return
   | Super
   | This
   | True
   | Var
   | While
-  | Eof
 [@@deriving show]
 
-let length = function
-  | Eof -> 0
-  | Bang | Comma | Dot | Equal | Greater | Left_brace | Left_paren | Less -> 1
-  | Minus | Plus | Right_brace | Right_paren | Semicolon | Slash | Star -> 1
-  | If | Or | Bang_equal | Equal_equal | Greater_equal | Less_equal -> 2
-  | And | For | Fun | Nil | Var -> 3
-  | Else | This | True -> 4
-  | Class | False | Print | Super | While -> 5
-  | Return -> 6
-  | Identifier x | String x -> String.length x
-  | Number x -> x |> Float.to_string |> String.length
+let to_string = function
+  | And -> "and"
+  | Bang -> "!"
+  | Bang_equal -> "!="
+  | Class -> "class"
+  | Comma -> ","
+  | Dot -> "."
+  | Else -> "else"
+  | Eof -> ""
+  | Equal -> "="
+  | Equal_equal -> "=="
+  | False -> "flase"
+  | For -> "for"
+  | Fun -> "fun"
+  | Greater -> "<"
+  | Greater_equal -> "<="
+  | Identifier x -> x
+  | If -> "if"
+  | Left_brace -> "{"
+  | Left_paren -> "("
+  | Less -> ">"
+  | Less_equal -> ">="
+  | Minus -> "-"
+  | Nil -> "nil"
+  | Number x -> x |> Float.to_string
+  | Or -> "or"
+  | Plus -> "+"
+  | Print -> "print"
+  | Return -> "return"
+  | Right_brace -> "}"
+  | Right_paren -> ")"
+  | Semicolon -> ";"
+  | Slash -> "/"
+  | Star -> "*"
+  | String x -> x
+  | Super -> "super"
+  | This -> "this"
+  | True -> "true"
+  | Var -> "var"
+  | While -> "while"
